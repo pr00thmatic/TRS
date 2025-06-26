ed5b7e7db8d49346e6797712211bc63b884f9e5b
Ruth García <pr00thmatic@gmail.com>
2025-06-26 19:10:40 -0400

    (wip) add a silly IK controller for opening doors

---
+++ Assets/Scripts/Shared/PlayerReferences.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerReferences : MonoBehaviour {
+  public TwoBoneIKConstraint leftArm;
+  public TwoBoneIKConstraint rightArm;
+}

45ebbec173393d235d431d75b75a80aeaf2d1805
Ruth García <pr00thmatic@gmail.com>
2025-06-26 16:47:46 -0400

    improve inside motion and walking animations

--- Assets/Scripts/Inside/InsidePlayerMotion.cs
+++ Assets/Scripts/Inside/InsidePlayerMotion.cs
@@ -2,21 +2,38 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
+[RequireComponent(typeof(SoftForwardTracker))]
 public class InsidePlayerMotion : MonoBehaviour {
+  [Header("Initialization")]
   public InsideSurveillanceSystem surveillanceSystem;
+  public Animator animator;
+  public SoftForwardTracker forwardTracker;
+
+  [Header("Configuration")]
   public float speed = 1;
   public float animSpeed = 0.5f;
-  public float currentSpeed = 0;
-  public Animator animator;
-  Vector3 direction;
+
+  [Header("Information")]
+  public Vector3 direction;
+  public Vector3 lastPosition; // to prevent it from doing the walk animation when walking into a wall
+  public float currentSpeed;
+
+  void Start () => lastPosition = transform.position;
+
+  void OnValidate () {
+    animator = GetComponentInChildren<Animator>();
+    surveillanceSystem = FindFirstObjectByType<InsideSurveillanceSystem>();
+    forwardTracker = GetComponent<SoftForwardTracker>();
+  }
 
   void FixedUpdate () {
-    direction = (Input.GetAxis("Horizontal") * surveillanceSystem.Right + Input.GetAxis("Vertical") * surveillanceSystem.Forward);
-    currentSpeed = (direction * speed).magnitude;
+    direction = Vector3.ClampMagnitude((Input.GetAxis("Horizontal") * surveillanceSystem.Right +
+                                        Input.GetAxis("Vertical") * surveillanceSystem.Forward), 1);
     transform.position += direction * speed * Time.deltaTime;
+    currentSpeed = (transform.position - lastPosition).magnitude / Time.deltaTime;
+    animator.SetFloat("speed", currentSpeed * animSpeed);
 
-    if (currentSpeed > 0.01) transform.forward = direction;
-
-    animator.SetFloat("speed", currentSpeed / animSpeed);
+    if (direction.magnitude != 0) forwardTracker.targetForward = direction;
+    lastPosition = transform.position;
   }
 }

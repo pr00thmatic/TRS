4a97b270b9354e8cb3197f622fcca93e60ceb525
Ruth García <pr00thmatic@gmail.com>
2025-07-09 11:42:27 -0400

    getting the gist of character controller now

--- Assets/Scripts/TopDown/TopDownMovementControl.cs
+++ Assets/Scripts/TopDown/TopDownMovementControl.cs
@@ -1,49 +1,43 @@
 using UnityEngine;
-using UnityEngine.AI;
 using Shared;
 
 namespace Realities.TopDown {
-  public partial class TopDownMovementControl : MonoBehaviour {
+  public class TopDownMovementControl : MonoBehaviour {
     public const float DISTANCE_EPSILON = 0.1f;
 
     [Header("Initialization")]
-    public SurveillanceSystem surveillanceSystem;
-    public Animator animator;
-    public LazyAim lazyAim;
-    public NavMeshAgent agent;
+    [SerializeField] private SurveillanceSystem surveillanceSystem;
+    [SerializeField] private Animator animator;
+    [SerializeField] private LazyAim lazyAim;
+    [SerializeField] private CharacterController controller;
 
     [Header("Configuration")]
-    public float speed = 2;
-    public float animationSmoothnessSpeed = 3;
+    [SerializeField] private float speed = 2;
+    [SerializeField] private float animationSmoothnessSpeed = 3;
 
     [Header("Information")]
-    public Vector3 commandedDirection;
-    public float desiredSpeed;
-    public float characterSpeed;
-    public float animationSpeed;
-    public Vector3 lastPosition;
+    [SerializeField] private Vector3 commandedDirection;
+
+    public float AnimationSpeed { get => animator.GetFloat("speed"); set => animator.SetFloat("speed", value); }
+    public float CharacterSpeed => controller.velocity.magnitude;
+    public float DesiredSpeed => commandedDirection.magnitude * speed;
 
     void Reset () {
       animator = GetComponentInChildren<Animator>();
       surveillanceSystem = FindFirstObjectByType<SurveillanceSystem>();
       lazyAim = GetComponent<LazyAim>();
-      agent = GetComponent<NavMeshAgent>();
+      controller = GetComponent<CharacterController>();
     }
 
     void FixedUpdate () {
-      lastPosition = agent.transform.position;
-      agent.Move(agent.transform.forward * desiredSpeed * Time.deltaTime);
-      characterSpeed = (agent.transform.position - lastPosition).magnitude / Time.deltaTime;
+      controller.Move(controller.transform.forward * DesiredSpeed * Time.fixedDeltaTime);
     }
 
     void Update () {
       commandedDirection = Vector3.ClampMagnitude(Input.GetAxis("Horizontal") * surveillanceSystem.Right +
                                                   Input.GetAxis("Vertical") * surveillanceSystem.Forward, 1);
-      desiredSpeed = commandedDirection.magnitude * speed;
-      if (desiredSpeed > DISTANCE_EPSILON) lazyAim.targetForward = commandedDirection.normalized;
-
-      animationSpeed = Mathf.MoveTowards(animationSpeed, characterSpeed, animationSmoothnessSpeed * Time.deltaTime);
-      animator.SetFloat("speed", animationSpeed);
+      if (DesiredSpeed > DISTANCE_EPSILON) lazyAim.targetForward = commandedDirection.normalized;
+      AnimationSpeed = Mathf.MoveTowards(AnimationSpeed, CharacterSpeed, animationSmoothnessSpeed * Time.deltaTime);
     }
   }
 }
